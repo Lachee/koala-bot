c0c42cf9668c23ddbaf2fe0747d97c1906044a59
Lachee <lachlan@hotmail.com>
2019-08-26 20:25:37 +1000

    fixed config permissions for the help

--- KoalaBot/Modules/Configuration/ConfigurationModule.cs
+++ KoalaBot/Modules/Configuration/ConfigurationModule.cs
@@ -14,6 +14,7 @@ using KoalaBot.CommandNext;
 namespace KoalaBot.Modules.Configuration
 {
     [Group("config")]
+    [Permission("koala.config")]
     public partial class ConfigurationModule : BaseCommandModule
     {
         public Koala Bot { get; }
