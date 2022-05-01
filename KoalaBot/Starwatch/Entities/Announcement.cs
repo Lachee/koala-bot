443690a178a52a9fff4c3ae92c18b89aa12ecf9b
- <56451580+MiniOranges@users.noreply.github.com>
2022-05-01 18:47:20 -0500

    Add Announcement entities

---
+++ KoalaBot/Starwatch/Entities/Announcement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KoalaBot.Starwatch.Entities
+{
+    public class Announcement
+    {
+        public string Message { get; set; }
+        public bool Enabled { get; set; }
+        public double Interval { get; set; }
+    }
+}
---
+++ KoalaBot/Starwatch/Entities/AnnouncementPatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KoalaBot.Starwatch.Entities
+{
+    public class AnnouncementPatch
+    {
+        public string Message { get; set; }
+        public bool? Enabled { get; set; }
+        public double? Interval { get; set; }
+    }
+}
