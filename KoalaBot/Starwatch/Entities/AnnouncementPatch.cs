27c6f0ad006acd291652eb0f9641237163145840
- <56451580+MiniOranges@users.noreply.github.com>
2022-05-01 19:06:18 -0500

    Add Id to AnnouncementPatch

--- KoalaBot/Starwatch/Entities/AnnouncementPatch.cs
+++ KoalaBot/Starwatch/Entities/AnnouncementPatch.cs
@@ -6,6 +6,7 @@ namespace KoalaBot.Starwatch.Entities
 {
     public class AnnouncementPatch
     {
+        public int Id { get; set; }
         public string Message { get; set; }
         public bool? Enabled { get; set; }
         public double? Interval { get; set; }
